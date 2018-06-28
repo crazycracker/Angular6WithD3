f459d909a979922c63665d2cc2dfadc9a11abb83
Manchala <vinay.manchala@capgemini.com>
2018-06-28 14:24:28 +0530

    project done

---
+++ d3project/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  private chartData: Array<any>;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.generateData();
+  }
+
+  generateData() {
+    this.chartData = [];
+    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
+      this.chartData.push([
+        `Index ${i}`,
+        Math.floor(Math.random() * 100)
+      ]);
+    }
+  }
+}
